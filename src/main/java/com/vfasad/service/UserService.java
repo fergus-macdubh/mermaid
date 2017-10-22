fddb122ae50c9480be63df3031fd31f408f2838b
Fergus <fergusmacdubh@gmail.com>
2017-10-22 20:44:18 +0300

    Classic env vars

--- src/main/java/com/vfasad/service/UserService.java
+++ src/main/java/com/vfasad/service/UserService.java
@@ -4,15 +4,10 @@ import com.vfasad.entity.User;
 import com.vfasad.exception.NotFoundException;
 import com.vfasad.repo.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
@@ -38,25 +33,7 @@ public class UserService {
         User user = userRepository.getByEmail(details.get("email"))
                 .orElse(new User(details.get("email"), "Незнакомец", "Незнакомец", "", "", "", ""));
 
-        updateGrantedAuthorities(authentication, user.getRole());
-
         return user;
-    }
-
-    private void updateGrantedAuthorities(OAuth2Authentication authentication, String role) {
-        if (authentication.getAuthorities().size() == 0
-                || ((GrantedAuthority) authentication.getAuthorities().toArray()[0]).getAuthority().equals(role)) {
-            return;
-        }
-
-        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(
-                authentication.getPrincipal(),
-                authentication.getCredentials(),
-                StringUtils.isEmpty(role)
-                        ? Collections.emptyList()
-                        : Collections.singletonList(new SimpleGrantedAuthority(role)));
-        authentication = new OAuth2Authentication(authentication.getOAuth2Request(), token);
-        authentication.setDetails(authentication.getDetails());
     }
 
     public User updateUser(Map<String, String> details) {

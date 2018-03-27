9ffa0857afb436d52ab054754781ee3150eb96e4
Alesia <alesia.pavliuchenkova@gmail.com>
2018-03-27 15:08:14 +0300

    Cover OrderService with tests #56

--- src/test/java/com/vfasad/test/service/OptionServiceTest.java
+++ src/test/java/com/vfasad/test/service/OptionServiceTest.java
@@ -27,11 +27,10 @@ public class OptionServiceTest {
 
     @Test
     public void testFindAll() {
-        List<Option> optionList = generateOptionList(false);
-        when(optionRepository.findAll()).thenReturn(new ArrayList<Option>(){{add(new Option());}});
+        List<Option> optionList = generateOptionList();
+        when(optionRepository.findAll()).thenReturn(optionList);
         LinkedHashMap<OptionName, Option> expectedResult =
                 optionList.stream().collect(Collectors.toMap(Option::getName, Function.identity(), (o1,o2) -> o2, LinkedHashMap::new));
-        expectedResult.put(null, new Option());
 
         Collection<Option> resultOptionCollection = optionService.findAll();
         assertEquals("Invalid count in collection of Options", expectedResult.values().size(), resultOptionCollection.size());
@@ -42,28 +41,28 @@ public class OptionServiceTest {
 
     @Test
     public void testSave() {
-        List<Option> optionList = generateOptionList(true);
+        List<Option> optionList = generateOptionList();
         optionService.save(optionList);
         verify(optionRepository, times(1)).save(optionList);
     }
 
     @Test
     public void testGetOptionsMap() {
-        List<Option> optionList = generateOptionList(true);
+        List<Option> optionList = generateOptionList();
         Map<String, String> expectedOptionMap =
                 optionList.stream()
-                        .collect(Collectors.toMap(Option::getValue, Option::getValue));
+                        .collect(Collectors.toMap(o -> o.getName().toString(), Option::getValue));
         optionList.add(new Option());
         when(optionRepository.findAll()).thenReturn(optionList);
         Map<String, String> resultOptionMap = optionService.getOptionsMap();
         assertEquals("Invalid Map of Options", expectedOptionMap, resultOptionMap);
     }
 
-    private List<Option> generateOptionList(boolean withValues) {
+    private List<Option> generateOptionList() {
         List<Option> optionList = new ArrayList<>();
         for (OptionName optionName: OptionName.values()){
             Option option = new Option(optionName);
-            if (withValues) option.setValue(optionName.toString());
+            option.setValue(optionName.toString());
             optionList.add(option);
         }
         return optionList;

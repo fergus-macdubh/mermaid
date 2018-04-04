c7c6468980b00fc2c4b37aa4a90add498a1b6afc
Alesia <alesia.pavliuchenkova@gmail.com>
2018-04-04 17:36:31 +0300

    Move storage mail config to env variable #83

--- src/main/java/com/vfasad/service/EmailService.java
+++ src/main/java/com/vfasad/service/EmailService.java
@@ -3,6 +3,7 @@ package com.vfasad.service;
 import com.vfasad.entity.Order;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
@@ -11,6 +12,9 @@ import java.util.Map;
 @Slf4j
 @Service
 public class EmailService {
+    @Value("${mail.storage}")
+    private String mailStorage;
+
     @Autowired
     private EmailSender emailSender;
 
@@ -45,7 +49,7 @@ public class EmailService {
 
         // send copy to storage
         emailSender.send(
-                System.getenv("MAIL_STORAGE"),
+                mailStorage,
                 subject,
                 "email/order-flow.ftl",
                 model);

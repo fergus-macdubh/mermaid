8b08bd6214126ca41dfac2ef5e4e835f4d18c394
Tamara <tamara.cherniaieva@gmail.com>
2020-12-08 19:18:38 +0200

    The final stage of Artifact_150

--- src/main/java/com/vfasad/service/PrintService.java
+++ src/main/java/com/vfasad/service/PrintService.java
@@ -2,48 +2,52 @@ package com.vfasad.service;
 
 import com.vfasad.entity.Order;
 import com.vfasad.entity.OrderConsume;
-import com.vfasad.entity.Product;
+import com.vfasad.entity.OrderStatus;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
-import java.util.*;
+import java.util.Date;
+import java.util.List;
 
 @Service
 public class PrintService {
+    final int FIRST_TABLE_ROW = 5;
+
+    @Value("classpath:templates/xlsx/ReportInProgress.xlsx")
+    private File xlsxReportTemplate;
+
     @Autowired
     private OrderService orderService;
 
-
     public byte[] generateInProgressXlsx() {
-        try {
-            FileInputStream file = new FileInputStream("C:\\Users\\Mirrada\\Desktop\\Report.xlsx");//take a template
-            XSSFWorkbook workbook = new XSSFWorkbook(file);//created the object of xlsx library
 
-            Sheet sheet = workbook.getSheetAt(0);//retrieve the first sheet
-            /*Row row = sheet.getRow(5);
-            Cell cell_A11 = row.getCell(0);
-            cell_A11.setCellValue(777);*/
+        try {
+            FileInputStream file = new FileInputStream(xlsxReportTemplate);
+            XSSFWorkbook workbook = new XSSFWorkbook(file);
+
+            Sheet sheet = workbook.getSheetAt(0);
 
             Row rowDate = sheet.getRow(1);
             Cell cellDate = rowDate.getCell(6);
             SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
             cellDate.setCellValue(format.format(new Date()));
 
-            /// templateMapping();
-            List<Order> list = templateMapping();
-            int i = 5;
-            int a = 0;
+            List<Order> list = orderService.findByStatus(OrderStatus.IN_PROGRESS);
+
+            int i = FIRST_TABLE_ROW;
             for (Order order : list) {
-                Cell cell = sheet.getRow(i).getCell(0);
-                cell.setCellValue(order.getId());
+                Cell cell0 = sheet.getRow(i).getCell(0);
+                cell0.setCellValue(order.getId());
                 Cell cell1 = sheet.getRow(i).getCell(1);
                 cell1.setCellValue(order.getDocument());
                 Cell cell2 = sheet.getRow(i).getCell(2);
@@ -59,66 +63,20 @@ public class PrintService {
                 }
             }
 
-            ByteArrayOutputStream report = new ByteArrayOutputStream();
-            workbook.write(report);
-            byte[] reportArray = report.toByteArray();
+            ByteArrayOutputStream reportInProgress = new ByteArrayOutputStream();
+            workbook.write(reportInProgress);
+            byte[] reportArray = reportInProgress.toByteArray();
 
             workbook.close();//
             file.close();
 
-            report.close();
+            reportInProgress.close();
             return reportArray;
 
         } catch (IOException e) {
             System.out.println(e);
             throw new RuntimeException();
         }
-
-    }
-
-    private List<Order> templateMapping() throws IOException {
-        Order order1 = new Order();
-        order1.setId(1L);
-        order1.setDocument("Gate");
-        order1.setArea(115);
-
-        OrderConsume orderConsume_Gate_green = new OrderConsume();//The First OrderConsume for order1
-        Product product_green = new Product();
-        product_green.setName("Colour_green");
-        product_green.setProducer("USA");
-        orderConsume_Gate_green.setProduct(product_green);
-        orderConsume_Gate_green.setCalculatedQuantity(45);
-
-        OrderConsume orderConsume_Gate_grey = new OrderConsume();//The Second OrderConsume for order1
-        Product product_grey = new Product();
-        product_grey.setName("Colour_grey");
-        product_grey.setProducer("Ukraine");
-        orderConsume_Gate_grey.setProduct(product_grey);
-        orderConsume_Gate_grey.setCalculatedQuantity(91);
-
-        Set<OrderConsume> set_orde1 = new HashSet<>();
-        set_orde1.add(orderConsume_Gate_green);
-        set_orde1.add(orderConsume_Gate_grey);
-        order1.setConsumes(set_orde1);
-
-
-        Order order2 = new Order();
-        order2.setId(2L);
-        order2.setDocument("House");
-        order2.setArea(1225);
-
-        OrderConsume orderConsume_house_green = new OrderConsume();
-        orderConsume_house_green.setProduct(product_green);
-        orderConsume_house_green.setCalculatedQuantity(3000);
-        Set<OrderConsume>set_order2 = new HashSet<>();
-        set_order2.add(orderConsume_house_green);
-        order2.setConsumes(set_order2);
-
-        List<Order> list = new ArrayList<>();
-        list.add(order1);
-        list.add(order2);
-
-        return list;
     }
 }
 
